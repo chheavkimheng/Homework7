2db244e49f87e8bfd7a513454bacf3ab5dd0a64d
chheavkimheng <156335215+chheavkimheng@users.noreply.github.com>
2024-04-07 23:42:27 +0700

    commit

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,9 @@
 def remove_all_after(numbers, n):
-    ...
+    try:
+        index = numbers.index(n)  
+        return numbers[:index + 1] 
+    except ValueError:
+        return numbers
+
+print(remove_all_after([1, 2, 3, 4, 5], 3))  
+print(remove_all_after([1, 1, 2, 2, 3, 3], 2))  
