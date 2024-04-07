a26386b69f4d2344f87cf4a8f9818ec24265b3fa
ChandarabothSeng <144997059+ChandarabothSeng@users.noreply.github.com>
2024-04-07 23:35:24 +0700

    commit

--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,13 @@
 def remove_all_after(numbers, n):
     ...
+def remove_all_after(numbers, n):
+    if not numbers:
+        return []
+    try:
+        idx = numbers.index(n)
+        return numbers[:idx+1]
+    except ValueError:
+        return numbers
+
+print(remove_all_after([1, 2, 3, 4, 5], 4))    
+print(remove_all_after([1, 2, 2, 2, 3, 3], 3))  
