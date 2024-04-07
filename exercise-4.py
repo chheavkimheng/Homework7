2a876aa301903236b44369c7ff734af4e98244b0
ChandarabothSeng <144997059+ChandarabothSeng@users.noreply.github.com>
2024-04-07 23:11:22 +0700

    commit

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,12 @@
 def chunking_by(numbers, chunck):
-    ...
+    if not numbers:
+        return []
+    
+    chunks = []
+    
+    for i in range(0, len(numbers), chunck):
+        chunks.append(numbers[i:i + chunck])
+    
+    return chunks
+print(chunking_by([8, 2, 7, 5, 1, 0, 2, 6, 3, 7], 3))  
+print(chunking_by([4, 2, 5, 3, 5, 8], 2))             
