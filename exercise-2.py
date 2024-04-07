18ba12e3d6cbd833921a2fd071dae4e116a0a1df
chheavkimheng <156335215+chheavkimheng@users.noreply.github.com>
2024-04-07 23:34:24 +0700

    commit

--- exercise-2.py
+++ exercise-2.py
@@ -1,2 +1,11 @@
 def index_power(numnbers, n):
-    ...
+
+    if 0 <= n < len(numnbers):
+        return numnbers[n] ** n
+    else:
+        return -1
+
+print(index_power([1, 2, 3, 4], 2))
+print(index_power([1, 3, 10, 100], 3))
+print(index_power([0, 1], 0))
+print(index_power([1, 2], 3))
